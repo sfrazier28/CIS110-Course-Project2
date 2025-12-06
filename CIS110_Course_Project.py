09a1d6ab1a4233ad6f0bc2a18190254235690078
username <useremail>
2025-12-05 20:04:47 -0800

    friday

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -1,1 +1,13 @@
+print(f"Welcome...")
+print(f"I would love to share a story with you about a turtle that needs to make a decision that will shape it's life forever!")
+print(f"First, lets get some details from you..")
+print(f"Make sure to press 'Enter' after your responses")
+input(f"\nPress the enter key to continue...")
 
+color = input("\nWhat is your least favorite color:  ")
+name = input("\nWhat is the turtle's name :  ")
+location = input("\nWhat is your dream destination:  ")
+age = input("\nWhat is your favorite number:  ")
+emotion = input("\nWhat is your current mood:  ")
+
+print(f"Let us begin")
